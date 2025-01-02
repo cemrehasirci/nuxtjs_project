f524f0a1b348b182a2b19e318a834aa43a46978d
Cemre HASIRCI <cemrehasirci@hotmail.com>
2025-01-03 00:42:58 +0300

    yapı1

---
+++ components/header.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <header class="header">
+            Aradığınız Kitabın Adresi 📚 İndirimler ve Kampanyalar 👍 18 Yıldır Güvenle
+        </header>
+      
+        <div class="top-bar">
+  
+            <div class="logo">
+                <a href="#">
+                <img src="/images/logo.svg" alt="Logo" />
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.header {
+  background-color: #c9a046;
+  color: #fff; 
+  text-align: center;
+  padding: 10px 0; 
+  font-size: 25px; 
+}
+
+.top-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  padding: 10px 20px;
+  border-bottom: 1px solid #ddd;
+}
+
+.logo img {
+  margin-right: 30px;
+  width: 120px;
+}
